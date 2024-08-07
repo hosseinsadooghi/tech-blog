846413334caa6c9e1f3c1503a35714712a2912cd
hosseinsadooghi <hosseon.sadoghi@gmail.com>
2024-08-07 12:05:19 -0700

    learning about base url

--- lib/services/dio_service.dart
+++ lib/services/dio_service.dart
@@ -4,13 +4,14 @@ class DioService {
   Future<dynamic> getMethod(String url) async {
     Dio dio = Dio();
     dio.options.headers['content-type'] = 'application/json';
-    return await dio
-        .get(url,
-            options: Options(
-              responseType: ResponseType.json,
-              method: 'GET',
-            ))
-        .then((response) {
+    return await dio.get(
+      url,
+      options: Options(
+        responseType: ResponseType.json,
+        method: 'GET',
+      )
+    )
+    .then((response) {
       return response;
     });
   }
