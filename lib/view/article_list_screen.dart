dafc1b54a3772989957cf113d03a10f16525b718
hosseinsadooghi <hosseon.sadoghi@gmail.com>
2024-08-21 04:12:52 -0700

    making the first demo of article list

--- lib/view/article_list_screen.dart
+++ lib/view/article_list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,19 +13,76 @@ class ArticleListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+    var size = MediaQuery.of(context).size;
+    var textTheme = Theme.of(context).textTheme;
+    var marginFromSide = size.width / 12.46;
     return SafeArea(
         child: Scaffold(
-          backgroundColor: MyColors.scafoldBackGround,
-          appBar: techBlogAppBar(context,MyStrings.articlesList),
-          body: Obx(
+      backgroundColor: MyColors.scafoldBackGround,
+      appBar: techBlogAppBar(context, MyStrings.articlesList),
+      body: Padding(
+        padding: EdgeInsets.all(marginFromSide),
+        child: SizedBox(
+          child: Obx(
             () => ListView.builder(
-              itemCount: articleControler.articleList.length,
-              itemBuilder: (context,index){
-                
-              }
-            ),
+                scrollDirection: Axis.vertical,
+                itemCount: articleControler.articleList.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.fromLTRB(0,8,0,8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: Get.width/3,
+                          height: Get.height/10,
+                          child: CachedNetworkImage(
+                            imageUrl: articleControler.articleList[index].image!,
+                            imageBuilder: ((context, imageProvider) {
+                              return Container(
+                                decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(16)),
+                                    image: DecorationImage(
+                                        image: imageProvider, fit: BoxFit.cover)),
+                              );
+                            }),
+                            placeholder: ((context, url) => loading()),
+                            errorWidget: (context, url, error) {
+                              return Icon(
+                                Icons.image_not_supported,
+                                size: 50,
+                              );
+                            },
+                          ),
+                        ),
+                        SizedBox(width: 16,),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: Get.width/2.5,
+                              child: Text(articleControler.articleList[index].title!,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(articleControler.articleList[index].author!),
+                                SizedBox(width: 20,),
+                                Text(articleControler.articleList[index].view!+" بازدید "),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                }),
           ),
+        ),
+      ),
     ));
   }
 }
