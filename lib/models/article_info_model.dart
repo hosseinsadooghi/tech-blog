a6c77c844b6eed8f86c53a4a3dc7b5ac39aa66b1
hosseinsadooghi <hosseon.sadoghi@gmail.com>
2024-08-30 09:17:29 -0700

    creating article info model

---
+++ lib/models/article_info_model.dart
@@ -0,0 +1,29 @@
+class ArticleInfoModel {
+  String? id;
+  String? title;
+  String? content;
+  String? image;
+  String? catId;
+  String? catName;
+  String? author;
+  String? view;
+  String? status;
+  String? createdAt;
+  bool? isFavorite;
+  ArticleInfoModel();
+  ArticleInfoModel.fromJson(Map<String, dynamic> element) {
+    var info = element['info'];
+    id= info['id'];
+    title=info['title'];
+    content=info['content'];
+    image=info['image'];
+    catId=info['cat_id'];
+    catName=info['cat_name'];
+    author=info['author'];
+    view=info['view'];
+    status=info['status'];
+    createdAt=info['created_at'];
+    
+    isFavorite= element['is_favorite'];
+  }
+}
